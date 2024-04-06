35b348f0285d7216cc9c8584e0f3b4e4d70a69ec
Alumno <Alumno@LAB1504-19>
2024-04-06 16:37:38 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio4/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Laboratorio4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+        private string connectionString = "Data Source=LAB1504-19\\SQLEXPRESS;Initial Catalog=Neptuno;User Id=TecsupAriana;Password=1234567";
+        private DataTable dataTable = new DataTable();
+
+       
+
+        private void CargarDatos()
+        {
+            try
+            {
+                List<Proveedor> proveedores = new List<Proveedor>();
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("USP_ListarProveedores", connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        int idProveedor = reader.GetInt32(reader.GetOrdinal("idProveedor"));
+                        string nombreCompañia = reader.GetString(reader.GetOrdinal("nombreCompañia"));
+                        string nombreContacto = reader.GetString(reader.GetOrdinal("nombreContacto"));
+                        string cargocontacto = reader.GetString(reader.GetOrdinal("cargocontacto"));
+                        string direccion = reader.GetString(reader.GetOrdinal("direccion"));
+                        proveedores.Add(new Proveedor { idProveedor = idProveedor, nombreCompañia = nombreCompañia, nombreContacto = nombreContacto, cargocontacto = cargocontacto, direccion = direccion });
+                    }
+                    connection.Close();
+                }
+                dataGridProveedores.ItemsSource = proveedores;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ Laboratorio4/Producto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+public class Proveedor
+{
+    public int idProveedor { get; set; }
+    public string nombreCompañia { get; set; }
+    public string nombreContacto { get; set; }
+    public string cargocontacto { get; set; }
+    public string direccion { get; set; }
+}
+
+
